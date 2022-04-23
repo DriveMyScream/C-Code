a44efe8d2bbaa5c7368a99ef69841b7ae95148ec
Shubham Pilivkar <shubhampilivkar@gmail.com>
2022-04-23 12:43:21 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,254 +1,331 @@
 #include<iostream>
-#include<fstream>
+#include<ctime>
 #include<cstdlib>
-#include<vector>
-#include<map>
 using namespace std;
-#define MIN_BALANCE 100
-class deficient_funds{};
-class Cl_Accounts
+struct cl_patient
 {
-private:
-long Accnt_No;
-string client_fname;
-string client_lname;
-float client_balance;
-static long Nxt_Accnt_No;
-
-public:
-Cl_Accounts(){}
-Cl_Accounts(string fname,string lname,float client_balance);
-long getAccNo(){return Accnt_No;}
-string getFName(){return client_fname;}
-string getLName(){return client_lname;}
-float getBlnce(){return client_balance;}
-void Deposit(float amount);
-void Withdraw(float amount);
-static void setLstAccntNo(long Accnt_No);
-static long getLstAccntNo();
-friend ofstream & operator<<(ofstream &ofs,Cl_Accounts &acc);
-friend ifstream & operator>>(ifstream &ifs,Cl_Accounts &acc);
-friend ostream & operator<<(ostream &os,Cl_Accounts &acc);
+	char first_name[20];
+	char last_name[20];
+	char patient_add[200];
+    char patient_phonenumber[10];
+    char patient_cnic[100];
+    int patient_age;
+    char patient_gender[8];
+    char patient_bloodtype[5];
+    char patient_disease_pt[50];
+    int patient_id;
+    char patient_symptom[500];
+    char patient_diagnosis[500];
+    char patient_medicine[500];
+    char patient_addmission[30];
+	struct cl_patient *next;
+	bool diagnose_patient;
 };
-long Cl_Accounts::Nxt_Accnt_No=0;
-
-class Bank
+struct cl_patient *header,*last_partner;
+bool checking=true;
+int array[20]={0},v=0;
+bool check_id(int ch)
 {
-private:
-map<long,Cl_Accounts> accounts_cl;
-public:
-Bank();
-Cl_Accounts Cl_Open_Account(string fname,string lname,float balance);
-Cl_Accounts Cl_Balance_Enquiry(long Account_no);
-Cl_Accounts Deposit(long Account_no,float amt);
-Cl_Accounts Withdraw(long Account_no,float amt);
-void CloseAccount(long Account_no);
-void ShowAllAccounts();
-~Bank();
+  	    for(int y=0;y<v;y++)
+  	    {
+  	    	if(ch==array[y])
+  	    	{
+  	    		return true;
+			}
+		}
+		return false;
 };
+class Doctor
+{
+	public:
+	  void add()
+	  {
+
+	  	cl_patient *pat;
+	  	pat=new cl_patient;
+	  	pat->diagnose_patient=false;
+	  	cout<<"Enter Patient ID"<<endl;
+	  	cin>>pat->patient_id;
+	  	cout<<"Enter Patient First Name"<<endl;
+	  	cin>>pat->first_name;
+	  	cout<<"Enter Patient Last name"<<endl;
+	  	cin>>pat->last_name;
+	  	cout<<"Enter Patient Age"<<endl;
+	  	cin>>pat->patient_age;
+	  	cout<<"Enter Patient Gender"<<endl;
+	  	cin>>pat->patient_gender;
+	  	cout<<"Enter Patient Blood Group"<<endl;
+	  	cin>>pat->patient_bloodtype;
+	  	cout<<"Enter Patient Contact "<<endl;
+	  	cin>>pat->patient_phonenumber;
+	  	fflush(stdin);
+	  	cout<<"Enter Patient CNIC"<<endl;
+	  	cin.getline(pat->patient_cnic,100);
+	  	fflush(stdin);
+	  	cout<<"Enter Patient Address"<<endl;
+	  	cin.getline(pat->patient_add,1000);
+
+	  	pat->next=NULL;
+	  	array[v]=pat->patient_id;
+
+	  	v++;
+	  	if(checking)
+	{
+	    header=pat;
+	    last_partner=pat;
+	    checking=false;
+	}
+	else
+	{
+		last_partner->next=pat;
+		last_partner=pat;
+	}
+
+  }
+  void show()
+  {
+  	    int z;
+  	    cl_patient *cur=NULL,*previous=NULL;
+  	    previous=header;
+  	    cur=header;
+  	    int change;
+ 	    cout<<"Enter Patient ID"<<endl;
+  	    cin>>change;
+		if(check_id(change))
+		{
+  	    while(cur->patient_id!=change)
+  	    {
+  	    	previous=cur;
+  	    	cur=cur->next;
+  	   	  }
+
+  	 	cout<<"Enter Patient First Name"<<endl;
+	  	cout<<cur->first_name<<endl;
+	  	cout<<"Enter Patient Last Name"<<endl;
+	  	cout<<cur->last_name<<endl;
+	  	cout<<"Enter Patient ID"<<endl;
+	  	cout<<cur->patient_id<<endl;
+	  	cout<<"Enter Patient Age"<<endl;
+	  	cout<<cur->patient_age<<endl;
+	  	cout<<"Enter Patient Gender"<<endl;
+	  	cout<<cur->patient_gender<<endl;
+	  	cout<<"Enter Patient Blood Type"<<endl;
+	  	cout<<cur->patient_bloodtype<<endl;
+	  	cout<<"Enter Patient Contact "<<endl;
+	  	cout<<cur->patient_phonenumber<<endl;
+	  	cout<<"CNIC"<<endl;
+	  	cout<<cur->patient_cnic<<endl;
+	  	cout<<"Enter Patient Address"<<endl;
+	    cout<<cur->patient_add<<endl;
+	    cout<<"*********************************"<<endl;
+	    if(cur->diagnose_patient){
+	    cout<<"Enter Symptoms"<<endl;
+	    cout<<cur->patient_symptom<<endl;
+	    cout<<"Enter Diagnosis"<<endl;
+	    cout<<cur->patient_diagnosis<<endl;
+	    cout<<"Enter Medicines"<<endl;
+	    cout<<cur->patient_medicine<<endl;
+	    cout<<"Admit Required to Hospital?"<<endl;
+	    cout<<cur->patient_addmission<<endl;
+	   }}
+	   else
+	   {
+	   	cout<<"ID not present"<<endl;
+	   }
+}
+  void diagnosis()
+  {
+  	int z;
+    cl_patient *pat=NULL,*previous=NULL;
+    pat=new cl_patient;
+  	previous=header;
+    pat=header;
+  	int change;
+  	cout<<"Enter Patient ID"<<endl;
+  	cin>>change;
+	if(check_id(change)){
+		while(pat->patient_id!=change )
+	  	{
+	  		previous=pat;
+	  		pat=pat->next;
+		}
+		pat->diagnose_patient=true;
+		cout<<"Enter Symptoms"<<endl;
+		cin>>pat->patient_symptom;
+		cout<<"Enter Diagnosis"<<endl;
+		cin>>pat->patient_diagnosis;
+		cout<<"Enter Medicines"<<endl;
+		cin>>pat->patient_medicine;
+		cout<<"Admit Required to Hospital?"<<endl;
+		cin>>pat->patient_addmission;
+
+	}
+
+	else
+		cout<<"ID is not Registered\n";
+
+	}
+void deleteList()
+{
+	int counter=0;
+    cl_patient *cur=NULL,*previous=NULL;
+
+  	previous=header;
+  	cur=header;
+  	int change;
+	cout<<"Enter the ID: ";
+	cin>>change;
+	if(check_id(change)){
+
+		while(cur->patient_id!=change)
+  	    {
+  	    	previous=cur;
+  	    	cur=cur->next;
+  		}
+  		previous->next=cur->next;
+  		free(cur);
+
+
+      for(int v=0;v<v;v++)
+      {
+      	if(array[v]==change){
+      		for (int y=v;y<v;y++){
+      			array[y]=array[y+1];
+			  }
+			  v--;
+		  }
+	  }
+    cout<<"SUCCESSFULLY DELETED ALL NODES OF LINKED LIST\n";
+    }
+    else
+       cout<<"ID not present"<<endl;
+}
+void update_menu()
+{
+	    cout<<"Enter 1 Patient First Name"<<endl;
+	  	cout<<"Enter 2 Patient Last Name"<<endl;
+	  	cout<<"Enter 3 Patient ID"<<endl;
+	  	cout<<"Enter 4 Patient Age"<<endl;
+	  	cout<<"Enter 5 Patient Gender"<<endl;
+	  	cout<<"Enter 6 Patient Blood Type"<<endl;
+	  	cout<<"Enter 7 Patient Phone Number "<<endl;
+	  	cout<<"Enter 8 Patient CNIC"<<endl;
+      	cout<<"Enter 9 Patient Address"<<endl;
+	    cout<<"*********************************"<<endl;
+	    cout<<"Enter 10 Symptoms"<<endl;
+	    cout<<"Enter 11 Diagnosis"<<endl;
+	    cout<<"Enter 12 Medicines"<<endl;
+	    cout<<"Admit 13 Required to Hospital?"<<endl;
+}
+void update_data(){
+    cl_patient *cur=NULL,*previous=NULL;
+	cur=header;
+	previous=header;
+	int patient_id,change;
+	cout<<"Enter ID"<<endl;
+	cin>>patient_id;
+	if(check_id(patient_id)){
+		while(cur->patient_id!=patient_id)
+  	    {
+  	    	previous=cur;
+  	    	cur=cur->next;
+  		}
+  		update_menu();
+  		cout<<"Choose Number"<<endl;
+  		cin>>change;
+  		if(change==1){
+  			cout<<"Enter First Name"<<endl;
+  			cin>>cur->first_name;
+		  }
+		else if(change==2){
+			cout<<"Enter Second Name"<<endl;
+			cin>>cur->last_name;
+		}
+		else if(change==3){
+			cout<<"Enter ID"<<endl;
+			cin>>cur->patient_id;
+		}
+  		else if(change==4){
+  			cout<<"Enter Age"<<endl;
+  			cin>>cur->patient_age;
+		  }
+		else if(change==5){
+			cout<<"Enter Gender"<<endl;
+			cin>>cur->patient_gender;
+		}
+		else if(change==6){
+			cout<<"Enter Blood Type"<<endl;
+			cin>>cur->patient_bloodtype;
+		}
+		else if(change==7){
+			cout<<"Enter Phone Number"<<endl;
+			cin>>cur->patient_phonenumber;
+		}
+		else if(change==8){
+			cout<<"Enter CNIC"<<endl;
+			cin>>cur->patient_cnic;
+		}
+		else if(change==9){
+			cout<<"Enter Address"<<endl;
+			cin>>cur->patient_add;
+		}
+		else if(change==10){
+			cout<<"Enter 10 Symptoms"<<endl;
+			cin>>cur->patient_symptom;
+		}
+		else if(change==11){
+	    cout<<"Enter 11 Diagnosis"<<endl;
+	    cin>>cur->patient_diagnosis;
+	    }
+	    else if(change==12){
+	    cout<<"Enter 12 Medicines"<<endl;
+	    cin>>cur->patient_medicine;
+		}
+	    else if(change==13){
+	    cout<<"Admit 13 Required to Hospital?"<<endl;
+	    cin>>cur->patient_addmission;
+	}
+   }
+   else
+     cout<<"ID not present"<<endl;
+}
+};
+
+void display_menu()
+{
+	cout<<"|=================================|"<<endl;
+	cout<<"|[1] Add New Patient Record       |"<<endl;
+	cout<<"|[2] Add Patient Prescription     |"<<endl;
+	cout<<"|[3] Full History of the Patient  |"<<endl;
+	cout<<"|[4] Delete Patient Record        |"<<endl;
+	cout<<"|[5] Update Patient Record        |"<<endl;
+    cout<<"|=================================|"<<endl;
+	cout<<"Enter the number you want to Choose:"<<endl;
+}
+void menu()
+{
+	cout<<"press 1 To Doctor Menu"<<endl;
+	cout<<"press 2 To Patients Menu"<<endl;
+	cout<<"press 3 To Pharmacist Menu"<<endl;
+}
 int main()
 {
-Bank b;
-Cl_Accounts acc;
-int option;
-string fname,lname;
-long account_no;
-float blnced;
-float amnts;
-cout<<"***Bank Management System***"<<endl;
-do
-{
-cout<<"\n\tSelect One Option Below ";
-cout<<"\n\t1 Open an Account";
-cout<<"\n\t2 Balance Enquiry";
-cout<<"\n\t3 Deposit";
-cout<<"\n\t4 Withdrawal";
-cout<<"\n\t5 Close an Account";
-cout<<"\n\t6 Show All Accounts";
-cout<<"\n\t7 Quit";
-cout<<"\nEnter your choice: ";
-cin>>option;
-switch(option)
-{
-case 1:
-cout<<"Enter First Name: ";
-cin>>fname;
-cout<<"Enter Last Name: ";
-cin>>lname;
-cout<<"Enter Initial Balance: ";
-cin>>blnced;
-acc=b.Cl_Open_Account(fname,lname,blnced);
-cout<<endl<<"Congratulations Account is Created"<<endl;
-cout<<acc;
-break;
-case 2:
-cout<<"Enter Account Number:";
-cin>>account_no;
-acc=b.Cl_Balance_Enquiry(account_no);
-cout<<endl<<"Your Account Details"<<endl;
-cout<<acc;
-break;
-case 3:
-cout<<"Enter Account Number:";
-cin>>account_no;
-cout<<"Enter Balance:";
-cin>>amnts;
-acc=b.Deposit(account_no, amnts);
-cout<<endl<<"Amount is Deposited"<<endl;
-cout<<acc;
-break;
-case 4:
-cout<<"Enter Account Number:";
-cin>>account_no;
-cout<<"Enter Balance:";
-cin>>amnts;
-acc=b.Withdraw(account_no, amnts);
-cout<<endl<<"Amount Withdrawn"<<endl;
-cout<<acc;
-break;
-case 5:
-cout<<"Enter Account Number:";
-cin>>account_no;
-b.CloseAccount(account_no);
-cout<<endl<<"Account is Closed"<<endl;
-cout<<acc;
-case 6:
-b.ShowAllAccounts();
-break;
-case 7: break;
-default:
-cout<<"\nEnter corret choice";
-exit(0);
-}
-}while(option!=7);
-return 0;
-}
-Cl_Accounts::Cl_Accounts(string fname,string lname,float client_balance)
-{
-Nxt_Accnt_No++;
-Accnt_No=Nxt_Accnt_No;
-client_fname=fname;
-client_lname=lname;
-this->client_balance=client_balance;
-}
-
-void Cl_Accounts::Deposit(float amt)
-{
-client_balance+=amt;
-}
-void Cl_Accounts::Withdraw(float amt)
-{
-if(client_balance-amt<MIN_BALANCE)
-throw deficient_funds();
-client_balance-=amt;
-}
-void Cl_Accounts::setLstAccntNo(long Accnt_No)
-{
-Nxt_Accnt_No=Accnt_No;
-}
-long Cl_Accounts::getLstAccntNo()
-{
-return Nxt_Accnt_No;
-}
-ofstream & operator<<(ofstream &ofs,Cl_Accounts &acc)
-{
-ofs<<acc.Accnt_No<<endl;
-ofs<<acc.client_fname<<endl;
-ofs<<acc.client_lname<<endl;
-ofs<<acc.client_balance<<endl;
-return ofs;
-}
-ifstream & operator>>(ifstream &ifs,Cl_Accounts &acc)
-{
-ifs>>acc.Accnt_No;
-ifs>>acc.client_fname;
-ifs>>acc.client_lname;
-ifs>>acc.client_balance;
-return ifs;
-}
-ostream & operator<<(ostream &os,Cl_Accounts &acc)
-{
-os<<"First Name:"<<acc.getFName()<<endl;
-os<<"Last Name:"<<acc.getLName()<<endl;
-os<<"Account Number:"<<acc.getAccNo()<<endl;
-os<<"Balance:"<<acc.getBlnce()<<endl;
-return os;
-}
-
-Bank::Bank()
-{
-Cl_Accounts acnt;
-ifstream infile;
-infile.open("Bank.data");
-if(!infile)
-{
-//cout<<"Error in Opening! File Not Found!!"<<endl;
-return;
-}
-while(!infile.eof())
-{
-infile>>acnt;
-accounts_cl.insert(pair<long,Cl_Accounts>(acnt.getAccNo(),acnt));
-}
-Cl_Accounts::setLstAccntNo(acnt.getAccNo());
-infile.close();
-}
-Cl_Accounts Bank::Cl_Open_Account(string fname,string lname,float balance)
-{
-ofstream outfile;
-Cl_Accounts acnt(fname,lname,balance);
-accounts_cl.insert(pair<long,Cl_Accounts>(acnt.getAccNo(),acnt));
-outfile.open("Bank.data", ios::trunc);
-map<long,Cl_Accounts>::iterator itr;
-for(itr=accounts_cl.begin();itr!=accounts_cl.end();itr++)
-{
-outfile<<itr->second;
-}
-outfile.close();
-return acnt;
-}
-Cl_Accounts Bank::Cl_Balance_Enquiry(long Accnt_No)
-{
-map<long,Cl_Accounts>::iterator itr=accounts_cl.find(Accnt_No);
-return itr->second;
-}
-Cl_Accounts Bank::Deposit(long Accnt_No,float amt)
-{
-map<long,Cl_Accounts>::iterator itr=accounts_cl.find(Accnt_No);
-itr->second.Deposit(amt);
-return itr->second;
-}
-Cl_Accounts Bank::Withdraw(long Accnt_No,float amt)
-{
-map<long,Cl_Accounts>::iterator itr=accounts_cl.find(Accnt_No);
-itr->second.Withdraw(amt);
-return itr->second;
-}
-
-void Bank::CloseAccount(long Accnt_No)
-{
-map<long,Cl_Accounts>::iterator itr=accounts_cl.find(Accnt_No);
-cout<<"Account Deleted"<<itr->second;
-accounts_cl.erase(Accnt_No);
-}
-
-void Bank::ShowAllAccounts()
-{
-map<long,Cl_Accounts>::iterator itr;
-for(itr=accounts_cl.begin();itr!=accounts_cl.end();itr++)
-{
-cout<<"Account "<<itr->first<<endl<<itr->second<<endl;
-}
-}
-
-Bank::~Bank()
-{
-ofstream outfile;
-outfile.open("Bank.data", ios::trunc);
-map<long,Cl_Accounts>::iterator itr;
-for(itr=accounts_cl.begin();itr!=accounts_cl.end();itr++)
-{
-outfile<<itr->second;
-}
-outfile.close();
+	struct cl_patient* header=NULL;
+	Doctor m;
+	int v=0;
+	do{
+	display_menu();
+	cin>>v;
+	if(v==1)
+	m.add();
+	else if(v==2)
+	m.diagnosis();
+	else if(v==3)
+	m.show();
+	else if(v==4)
+	m.deleteList();
+	else if(v==5)
+	m.update_data();
+	}while(1);
+	return 0;
 }
